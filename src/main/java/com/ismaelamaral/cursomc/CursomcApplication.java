790e629735fca3d382b012a10125c84000a4f999
ismlynx <ismael.leao@gmail.com>
2018-02-25 22:35:06 -0300

    Criando operacao de instanciacao

--- src/main/java/com/ismaelamaral/cursomc/CursomcApplication.java
+++ src/main/java/com/ismaelamaral/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.ismaelamaral.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.ismaelamaral.cursomc.domain.Categoria;
+import com.ismaelamaral.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+	}
+	
+	
 }
 
