bdcc9500148b6080ba9a08be6652d98a7485dd0f
ismlynx <ismael.leao@gmail.com>
2018-02-25 22:15:09 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ismaelamaral/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.ismaelamaral.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ismaelamaral.cursomc.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
