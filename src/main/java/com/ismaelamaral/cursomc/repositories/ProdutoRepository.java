4a95272b9cf25bd6a38ab39db0e8f1e9279729ab
Ismael Amaral <ismael.leao@gmail.com>
2018-02-27 16:08:49 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/ismaelamaral/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.ismaelamaral.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ismaelamaral.cursomc.domain.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
