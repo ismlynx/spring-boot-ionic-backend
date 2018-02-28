6690e9f165639e98fff111e4cf2876d63887c6b1
Ismael Amaral <ismael.leao@gmail.com>
2018-02-28 11:40:10 -0300

    Estado e Cidade

---
+++ src/main/java/com/ismaelamaral/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.ismaelamaral.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ismaelamaral.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
