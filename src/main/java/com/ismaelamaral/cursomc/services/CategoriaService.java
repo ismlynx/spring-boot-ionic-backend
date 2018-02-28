f0e0b5cbe25f2c5f0fb5bd8c53bf458243d797f8
Ismael Amaral <ismael.leao@gmail.com>
2018-02-28 10:53:18 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ismaelamaral/cursomc/services/CategoriaService.java
+++ src/main/java/com/ismaelamaral/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.ismaelamaral.cursomc.domain.Categoria;
 import com.ismaelamaral.cursomc.repositories.CategoriaRepository;
+import com.ismaelamaral.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,12 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Categoria obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tipo: " + Categoria.class.getName());
+		}
+		
 		return obj;
 	}
 }
