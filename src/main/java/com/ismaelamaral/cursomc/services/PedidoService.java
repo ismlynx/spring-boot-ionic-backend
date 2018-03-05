be039322370a6e59b14f9b20dd99467a2928a7b8
Ismael Amaral <ismael.leao@gmail.com>
2018-03-05 14:37:58 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/ismaelamaral/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.ismaelamaral.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ismaelamaral.cursomc.domain.Pedido;
+import com.ismaelamaral.cursomc.repositories.PedidoRepository;
+import com.ismaelamaral.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		
+		Pedido obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		
+		return obj;
+	}
+}
